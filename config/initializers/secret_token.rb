c046c30081e17d21a0a0c9c64f10d712aca9caf5
rabierre <rabierre@gmail.com>
2013-11-22 14:39:13 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bliki::Application.config.secret_key_base = '5b9ced41c2c23986e7ba4fbe502607a31628d36977b4e3932c70314a6ffc3e50943c20fc6f2efddd0488c83526559874b0a0e852a541a99d326e3210c9f67ebc'

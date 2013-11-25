173f50d5d73158cccba5a3a6789ccd109f69c22d
rabierre <rabierre@gmail.com>
2013-11-26 01:24:04 +0900

    log in/out is available now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,18 @@ class SessionsController < ApplicationController
     end
 
     def create
-        # todo
-        render "new"
+        user =User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_url, :notice => "Logged in!"
+        else
+            flash.now.alert = "Invalid Email or Password"
+            render 'new'
+        end
     end
 
     def destroy
-        #todo
+        session[:user_id] = nil
+        redirect_to root_url, :notice => "Logged out!"
     end
 end

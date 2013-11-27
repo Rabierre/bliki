0b8b9ae0e4b7872a309ac2dd086888acc1bb8a8c
rabierre <rabierre@gmail.com>
2013-11-27 23:02:34 +0900

    now query get desc revisions in order

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -1,5 +1,6 @@
 class RevisionsController < ApplicationController
     def index
         @post = Post.find(params[:post_id])
+        @revisions = @post.revision.order("created_at desc")
     end
 end

67aa2cbf5a739699216d26e96638ee2c82b59541
rabierre <rabierre@gmail.com>
2013-12-03 10:58:57 +0900

    set default values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(title: 'Uncategorized')
+Post.create(title: 'Welcome', body: 'Hello')

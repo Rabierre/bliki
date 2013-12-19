dd52444318747c90f401d86f35b12eb288dfb988
rabierre <rabierre@gmail.com>
2013-12-19 09:39:58 +0900

    add constraint

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
     #has_one :category, :primary_key => 'name'
-    belongs_to :cateogry,:foreign_key => 'name'
+    belongs_to :category, :foreign_key => 'name'
     has_many :revision, dependent: :destroy
     validates :title, presence: true, length: {minimum: 1}
 end
